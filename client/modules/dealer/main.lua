b688d726c3e1f8810f7ddc4cd69bbef8405f1f65
Georgi Stoyanov <stoyanow2707@gmail.com>
2023-05-18 11:56:52 +0300

    Removed ox_target index from the label (only ox)

--- client/modules/dealer/main.lua
+++ client/modules/dealer/main.lua
@@ -2,7 +2,11 @@ CreateThread(function()
     for index, value in pairs(Config.DealerLocations) do
         if value.interaction.type == 'target' then
             local Lang = Config.Languages[Config.Language];
-            AddTargetInteraction(Lang.DealerInteract..index, 'fas fa-cannabis', 'king_drugs_dealer_target_'..index, {
+            local text = Lang.DealerInteract..index;
+            if Config.TargetType == 'ox_target' then
+                text = Lang.DealerInteract;
+            end
+            AddTargetInteraction(text, 'fas fa-cannabis', 'king_drugs_dealer_target_'..index, {
                 size = value.interaction.size,
                 debug = value.interaction.debug,
                 coords = value.interaction.coords or value.coords,
--- config/config.lua
+++ config/config.lua
@@ -7,7 +7,7 @@ Config.Framework = 'qbcore'; -- qbcore or esx (ox_core soon)
 Config.Language = 'en'; -- en or bg
 Config.ZoneType = 'ox'; -- ox (ox_lib zones), poly (bt-polyzone) or distance (fivem)
 Config.ControlInteraction = 'ox'; -- ox, okokTextUI, customTextUI, 3DText
-Config.TargetType = 'qb-target'; -- ox_target, qtarget, bt-target or qb-target
+Config.TargetType = 'ox_target'; -- ox_target, qtarget, bt-target or qb-target
 Config.ContextType = 'ox'; -- ox (ox_lib context), nh-context, zf-context or qb-menu
 Config.Notifications = 'ox'; -- okok, ox, mythic or custom
 
@@ -74,8 +74,8 @@ Config.DealerLocations = {
         -- The Coords and Heading can be global for the table or for each option different
         interaction = {
             type = 'target', -- target or control
-            coords = vec3(-117.7423, 6348.8779, 31.4904),
-            heading = 50.7063,
+            coords = vec3(-384.27, 6080.07, 31.44),
+            heading = 305.0,
             debug = false, -- Only for target or ox zones
             size = vec3(1.5, 1.5, 2), -- Only for target or ox zones
             controlIdx = 38, -- Only for control
@@ -97,16 +97,48 @@ Config.DealerLocations = {
         },
         ped = { -- Table or false
             model = 'g_m_y_ballaeast_01',
-            coords = vec3(-117.7423, 6348.8779, 31.4904),
-            heading = 50.7063
+            coords = vec3(-384.27, 6080.07, 31.44),
+            heading = 305.7063
         },
         blip = { -- Table or false
             sprite = 51,
             color = 2,
             scale = 0.8,
             label = 'Dealer',
-            coords = vec3(-117.7423, 6348.8779, 31.4904)
+            coords = vec3(-384.27, 6080.07, 31.44)
         }
+    },
+    [2] = {
+        -- The Coords and Heading can be global for the table or for each option different
+        interaction = {
+            type = 'target', -- target or control
+            coords = vec3(-380.05, 6080.76, 31.44),
+            heading = 305.0,
+            debug = false, -- Only for target or ox zones
+            size = vec3(1.5, 1.5, 2), -- Only for target or ox zones
+            controlIdx = 38, -- Only for control
+            controlLabel = 'E' -- Only for control
+        },
+        prices = {
+            buy = { -- amount = 'infinite' --> infinite amount
+                [1] = { item ='heroin', price = 8, amount = 7 }, -- Add amount if you want to have a limit for buying
+                [2] = { item = 'marijuana', price = 15, amount = 12 },
+                [3] = { item = 'meth', price = 35, amount = 5 },
+                [4] = { item = 'cocaine', price = 60, amount = 2 }
+            },
+            sell = { -- amount = 'infinite' --> infinite amount
+                [1] = { item = 'heroin', price = 5, amount = 'infinite' }, -- Add amount if you want to have a maximum amount to sell
+                [2] = { item = 'marijuana', price = 10, amount = 4 },
+                [3] = { item = 'meth', price = 25, amount = 10 },
+                [4] = { item = 'cocaine', price = 50, amount = 15 }
+            }
+        },
+        ped = { -- Table or false
+            model = 'g_m_y_ballaeast_01',
+            coords = vec3(-380.05, 6080.76, 31.44),
+            heading = 130.0
+        },
+        blip = false
     }
 };
 
